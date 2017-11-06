70ad5787755dcacae1739cc484246bfa1ba9c695
regtak <maksim.shayakhmetov@gmail.com>
2017-11-06 07:06:46 +0000

    modified typo

--- manifests/base.pp
+++ manifests/base.pp
@@ -3,13 +3,13 @@ class profile::base(
     '0.ubuntu.pool.ntp.org',
     '1.ubuntu.pool.ntp.org',
   ],
+  Boolean $ntp_service_hasstatus = true,
   Boolean $ntp_service_enable = true
 ) {
   include ::ssh
   class {'::ntp': 
     servers => $ntp_servers,
     service_enable => $ntp_service_enable,
-    service_hasstatus => true,
   }
   
   if $facts['os']['family'] == 'RedHat' {
